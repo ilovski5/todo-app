b35d1965a20139288c27d84ccf445bb591c4ecb2
Marjan <ilovski5@gmail.com>
2021-04-10 10:08:04 +0200

    layout update: sticky footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import Layout from './components/Layout';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <Layout>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <p>Hello</p>
     </Layout>
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,17 +3,32 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+}));
 
 const Layout = ({ children }) => {
+  const classes = useStyles();
+
   return (
-    <>
+    <div className={classes.root}>
       <CssBaseline />
       <Navbar />
-      <Container maxWidth="sm">
+      <Container component="main" className={classes.main} maxWidth="sm">
         {children}
       </Container>
       <Footer />
-    </>
+    </div>
   );
 };
 
